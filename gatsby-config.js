c992d9a49345e313c1df1a96edc3be4bed6c26d3
doingz <allard@doingz.com>
2023-08-20 15:35:22 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `het lukt alleen met logistiek place`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
