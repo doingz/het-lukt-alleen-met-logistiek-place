9572bb9935042daafc3d1ac98354177b95479654
doingz <allard@doingz.com>
2023-08-21 10:05:19 +0200

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,13 +5,8 @@ import backgroundVideo from "../videos/hlaml-video.mp4";
 const IndexPage = () => {
   return (
     <div>
-      <div className="backgroundvideo">
-        <video className="video" playsInline loop autoPlay muted>
-          <source src={backgroundVideo} type="video/mp4" />
-        </video>
-        <div className="logo"></div>
-      </div>
       <h1>De website is onderweg, we gaan 28 augustus live!</h1>
+      <div className="logo"></div>
     </div>
   );
 };
